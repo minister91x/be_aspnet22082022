954bc210ffd651f719b08f280ebc1b55e5761a66
quan91 <quan91.nt@gmail.com>
2022-10-06 20:51:23 +0700

    Buoi17_Controller

---
+++ WebApplicationDemoMVC/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationDemoMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index(int? a, long? b)
+        {
+            //return View();
+
+            // return View();
+
+            return Json(new { stuatus = 1, description = " Trả về Json Thành công!" }, JsonRequestBehavior.AllowGet);
+
+            // return View();
+
+            var abc = GetById2(10);
+        }
+
+        [ActionName("find")] //   Chỉ định tên thay thế của phương thức hành động
+        public ActionResult GetByID(int id)
+        {
+            //return View();
+
+            // return View();
+
+            return Json(new { stuatus = id, description = "Lay du lieu cua id=" + id }, JsonRequestBehavior.AllowGet);
+
+            // return View();
+
+        }
+
+        [NonAction] // Chỉ định cho phương thức này không phải là phương thức hành động
+        public ActionResult GetById2(int id)
+        {
+            //return View();
+
+            // return View();
+
+            return Json(new { stuatus = id, description = "Lay du lieu cua id=" + id }, JsonRequestBehavior.AllowGet);
+
+            // return View();
+
+        }
+
+        
+        public ActionResult StudentInsertUpdate(int? id)
+        {
+            //return View();
+
+            // return View();
+            var StudentId = id != null ? Convert.ToInt32(id) : 0;
+
+            return Json(new { StudentId = StudentId, description = " Trả về Json Thành công!" }, JsonRequestBehavior.AllowGet);
+
+            // return View();
+
+        }
+
+        [HttpPost]
+        public JsonResult GetAllStudents()
+        {
+            int? a = null;
+
+            return Json(new { stuatus = 1, description = " Trả về Json Thành công!" }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
