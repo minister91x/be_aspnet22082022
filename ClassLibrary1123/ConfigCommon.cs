2e1cfa5fc817fbf2dd0fa3da31950a1af3e418e8
quan91 <quan91.nt@gmail.com>
2022-10-27 20:37:07 +0700

    Buoi22_FrontEnd

---
+++ ClassLibrary1123/ConfigCommon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1123
+{
+    public static class ConfigCommon
+    {
+        public static string Domain_URL
+        {
+            get
+            {
+                var root_url = System.Configuration.ConfigurationManager.AppSettings["DOMAIN_URL"] == null ? "http://localhost:51227" :
+                    System.Configuration.ConfigurationManager.AppSettings["DOMAIN_URL"].ToString();
+                return root_url;
+            }
+
+        }
+
+    }
+}
---
+++ WebApplicationDemoMVC/Scripts/Custom/Utils.js
@@ -0,0 +1,29 @@
+﻿window.Utils = {
+
+    ValidateData: function (tag_html_id, tag_html_notify) {
+
+        var html_tag_value = $("#" + tag_html_id).val();
+
+        console.log("username data:" + html_tag_value);
+
+        if (html_tag_value == null || html_tag_value == "") {
+            $("#" + tag_html_notify).show();
+            $("#" + tag_html_notify).text("trường này phải có dữ liệu")
+            $("#" + tag_html_id).css("border", "1px solid red");
+            $("#" + tag_html_id).focus();
+            return;
+        } else {
+            $("#" + tag_html_notify).hide();
+            $("#" + tag_html_id).css("border", "");
+
+        }
+    },
+
+    rootUrl: function () {
+        var rooturl = 'http://localhost:11339/';
+        if (location.host.toString().indexOf('localhost') >= 0) { rooturl = 'http://localhost:11339/'; }
+        if (location.host.toString().indexOf('islickdeals') >= 0) { rooturl = 'http://localhost:11339/'; }
+        return rooturl;
+    },
+  
+}
