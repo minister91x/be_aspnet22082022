99d0981cc491faca9d384c5c8bd2d9f33e24860e
quan91 <quan91.nt@gmail.com>
2022-11-10 20:26:20 +0700

    Buoi26_Identity_Owin

---
+++ ConsoleAppOwinDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleAppOwinDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:5000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ ConsoleAppOwinDemo/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+namespace ConsoleAppOwinDemo
+{
+    public class Startup
+    {
+        private IAppBuilder app;
+        public void Configuration(IAppBuilder app)
+        {
+#if DEBUG
+            app.UseErrorPage();
+#endif
+
+            app.Use(new Func<AppFunc, AppFunc>(next => (async env =>
+            {
+                Console.WriteLine("Begin Request");
+                foreach (var i in env.Keys)
+                {
+                    Console.WriteLine($"{i}\t={(env[i] == null ? "null" : env[i].ToString())}\t#\t{(env[i] == null ? "null" : env[i].GetType().FullName)}");
+                }
+                if (next != null)
+                {
+                    await next.Invoke(env);
+                }
+                else
+                {
+                    Console.WriteLine("Process Complete");
+                }
+
+
+            })));
+        }
+    }
+}
